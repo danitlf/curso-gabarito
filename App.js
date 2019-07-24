72203ebac96826cb0585dc6c332cb414e27f6b47
Daniel Tavares de Lima Freitas <daniel.lima@olxbr.com>
2019-07-23 23:21:06 -0300

    explicando state

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React, { Fragment, Component } from 'react';
-import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
+import {
+    SafeAreaView,
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity
+} from 'react-native';
 import { ComponentStateLess } from './src/components';
 
 function ComponentStateLessFunc({ text }) {
@@ -7,9 +13,22 @@ function ComponentStateLessFunc({ text }) {
 }
 
 class ComponentWithState extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { count: 0 };
+    }
+
+    changeText = () => {
+        this.setState({ count: ++this.state.count });
+    };
+
     render() {
         return (
             <View>
+                <TouchableOpacity onPress={this.changeText}>
+                    <Text>Button Troca</Text>
+                </TouchableOpacity>
+                <Text>{this.state.count}</Text>
                 <Text>
                     {this.props.children}, {this.props.outraProp}
                 </Text>
@@ -23,7 +42,7 @@ const App = () => {
         <Fragment>
             <SafeAreaView>
                 <ComponentStateLess text={'Testando algum texto'} />
-                <ComponentStateLessFunc text={'textooo'} />
+                {/* <ComponentStateLessFunc text={'textooo'} /> */}
                 <ComponentWithState outraProp={'outraProp'}>
                     Prop children
                 </ComponentWithState>
